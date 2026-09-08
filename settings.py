3a21e736d4992ddad33548dfab732f065fb8885f
SalzmannMark <salzmannmark@gmail.com>
2026-09-08 10:33:28 +0200

    changed settings.py

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ from fallbacks import pygame
 # board size (a smaller board may be useful at the beginning)
 COLS = 17
 ROWS = 17
-COIN_COUNT = 9
+COIN_COUNT = 50
 SCENARIOS = {
     # modes useful for agent development
 	"empty": {
@@ -16,11 +16,11 @@ SCENARIOS = {
     },
     "coin-heaven": {
         "CRATE_DENSITY": 0,
-        "COIN_COUNT": COIN_COUNT
+        "COIN_COUNT": 9
     },
     "loot-crate": {
         "CRATE_DENSITY": 0.75,
-        "COIN_COUNT": COIN_COUNT
+        "COIN_COUNT": 9
     },
      "loot-crate30": {
             "CRATE_DENSITY": 0.3,
